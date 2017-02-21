91e79432766142a59c11288420a5cf0882d6ea83
Artem Semenov <artem@semenov.ws>
2017-02-21 21:16:35 +0300

    Revert "Major commit"
    This reverts commit 84c7abe767bbe9ed3297cc638a089f00d89e1212.

--- site/web/app/themes/ksorokin/templates/header.php
+++ site/web/app/themes/ksorokin/templates/header.php
@@ -1,16 +1,16 @@
-<header class="pos-f-t">
-  <nav class="navbar navbar-light navbar-static-top bg-navbar">
+<header class="banner pos-f-t">
+  <nav class="navbar navbar-light navbar-static-top">
      <div class="container">
         <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-        <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar" aria-expanded="false" aria-controls="exCollapsingNavbar" aria-label="Toggle navigation"></button>
-        <div class="collapse navbar-toggleable-xs" id="exCollapsingNavbar">
-            <?php if (has_nav_menu('primary_navigation')) :
-                wp_nav_menu([
-                    'theme_location' => 'primary_navigation',
-                    'walker' => new wp_bootstrap_navwalker(),
-                    'menu_class' => 'float-md-right nav navbar-nav'
-                 ]);
-            endif; ?>
+          <button class="navbar-toggler hidden-md-up" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+           &#9776;
+          </button>
+        <div class="collapse navbar-toggleable-sm" id="navbarCollapse">
+        <?php
+         if (has_nav_menu('primary_navigation')) :
+            wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'float-md-right nav navbar-nav']);
+         endif;
+        ?>
         </div>
      </div>
   </nav>
