bf90d3a14d0621c23919114a12af058bd86ac9b6
Semen55338 <artem@semenov.ws>
2017-08-17 13:12:22 +0300

    update front-page

--- site/web/app/themes/ksorokin/front-page.php
+++ site/web/app/themes/ksorokin/front-page.php
@@ -1,21 +1,9 @@
 <?php while (have_posts()) : the_post(); ?>
 
-<?php
-
-$post_object = get_field('front-page-post');
-
-if( $post_object ):
-	// override $post
-	$post = $post_object;
-	setup_postdata( $post );
-
-?>
 
 <a class="home-banner" href="<?php the_permalink(); ?>">
   <img src="<?php the_post_thumbnail_url(); ?>">
 </a>
 
-<?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
-<?php endif; ?>
 
 <?php endwhile; ?>
