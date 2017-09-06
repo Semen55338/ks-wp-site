1dd4b0517d98335a6bc0195f89509f121956f038
Artem Semenov <artem@semenov.ws>
2017-09-06 10:09:42 +0300

    Change work images overlay

--- site/web/app/themes/ksorokin/templates/content-work.php
+++ site/web/app/themes/ksorokin/templates/content-work.php
@@ -3,7 +3,7 @@
     <?php the_post_thumbnail('thumbnail', array('class' => 'card-img')); ?>
     <div class="card-img-overlay">
       <p class="card-text"><?php echo count( get_field('work-gallery') ); ?> Images</p>
-      <h3 class="card-title text-xs-center"><?php the_title(); ?></h3>
+      <h5 class="card-title text-xs-center"><?php the_title(); ?></h5>
     </div>
   </a>
 </article>
