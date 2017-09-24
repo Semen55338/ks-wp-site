9e304721d7d59618ca1d3bf3dc0cb57e2e4d6642
Artem Semenov <artem@semenov.ws>
2017-09-24 20:03:12 +0300

    New front-page

--- site/web/app/themes/ksorokin/front-page.php
+++ site/web/app/themes/ksorokin/front-page.php
@@ -1,3 +1,9 @@
-<?php while (have_posts()) : the_post();
-bootstrap_carousel();
-endwhile; ?>
+<?php
+$query = new WP_Query( array( 'post_type' => 'work', 'orderby'   => 'rand', 'posts_per_page' => 1 ) );
+if ( $query->have_posts() ) :
+    while ( $query->have_posts() ) : $query->the_post(); ?>
+       <a class="home-banner" href="/work">
+          <img src="<?php the_post_thumbnail_url('large'); ?>">
+       </a>      
+    <?php endwhile; wp_reset_postdata();
+endif; ?>
